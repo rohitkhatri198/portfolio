2f16d90a6ea8683ea361663f206fb122e7a9e8e7
rohitprmsnls <rohit@prmsnls.xyz>
2023-03-31 20:02:23 +0530

    add download function

--- index.js
+++ index.js
@@ -98,3 +98,21 @@ button.addEventListener("clicked", (e) => {
     button.classList.remove(".pressed")
   },600)
 });
+
+function onButtonClick (){
+    console.log("hello")
+    // using Java Script method to get PDF file
+    fetch('Resume.pdf').then(response => {
+        response.blob().then(blob => {
+            // Creating new object of PDF file
+            const fileURL = window.URL.createObjectURL(blob);
+            // Setting various property values
+            let alink = document.createElement('a');
+            alink.href = fileURL;
+            alink.download = 'Resume.pdf';
+            alink.click();
+        })
+    })
+
+
+}
